1e8c71904fc001acee02110b9d839d18aa66b39e
Virendra Agarwal <vkagusa@yahoo.com>
2022-04-15 15:28:45 +0000

    Added the second label

--- main.tf
+++ main.tf
@@ -47,10 +47,7 @@ resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
   machine_type = var.machine_type
-  labels       = {
-    "billable" = "true"
-    }
-
+  
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-1804-lts"
@@ -71,6 +68,7 @@ resource "google_compute_instance" "hashicat" {
 
   labels = {
     name = "hashicat"
+    billable = "true"
   }
 
 }
