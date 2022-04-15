5d8c55e918598d5b90fdda6302499a92463f9ea3
Virendra Agarwal <vkagusa@yahoo.com>
2022-04-15 15:19:38 +0000

    Added the second label

--- main.tf
+++ main.tf
@@ -48,11 +48,6 @@ resource "google_compute_instance" "hashicat" {
   zone         = "${var.region}-b"
   machine_type = var.machine_type
   
-
-  labels {
-    "Billable": "true"
-    }
-
   
   boot_disk {
     initialize_params {
